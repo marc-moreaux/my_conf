07721819d48e6d046410927566b2252b6d9a2849
Marc <mr.moreaux@gmail.com>
2019-06-04 09:50:44 +0200

    move screen conf to shellScript and shift+m & HDDsetup on fstab

--- i3local.sh
+++
@@ -1,6 +0,0 @@
-
-xrandr --output VGA1 --properties --mode 1920x1080_60.00 --right-of LVSD1
-mkdir /media/mmoreaux/Data
-mkdir /media/mmoreaux/windows
-mount /dev/sda3 /media/mmoreaux/Data -o gid=mmoreaux -o uid=mmoreaux -o fmask=133 -o dmask=002
-mount /dev/sda2 /media/mmoreaux/windows -o gid=mmoreaux -o uid=mmoreaux -o fmask=133 -o dmask=002
